f1d66f88e5f822dc1b10b85d37c3fcaf0cdb2951
TreasureMaster <54545559+TreasureMaster@users.noreply.github.com>
2019-08-28 09:33:28 +0300

    Update-7: Delete Common File for StairElement

--- StairElement/common_file.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace StairElement;
-
-// тест
-// $elm = new Handrail(5);
-// echo "Имя элемента: {$elm->getElementName()}<br>";
-// echo "Общий объем элемента: {$elm->getTotalAmount()}<br>";
-?>
